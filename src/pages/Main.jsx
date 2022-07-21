7a2bdcd1e1f37a383ad641a3c6237cb814e11c05
chweyun <chweyun>
2022-07-21 22:40:47 +0900

    test

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,13 +5,13 @@ import "/Users/choiyun/post_eat_me/src/pages.css"
 const main = "POST-EAT\nME!";
 const Main = () => {
     return (
-        <div>
+        <>
             <div class="mainText">{main}</div>
             <button class="startBtn">
                 <Link to="/MainLogin" style={{textDecoration: 'none', color: 'black', backgroundColor: '#f0d596'}}>START</Link>
             </button>
             <div class="image"></div>
-        </div>
+        </>
     )
 };
 
